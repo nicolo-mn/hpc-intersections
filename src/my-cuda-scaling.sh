91da6386d79074aa9c2c6d3cc078b31b3cc16208
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2024-01-25 14:14:56 +0100

    modified script for cuda performance testing

--- src/my-cuda-scaling.sh
+++ src/my-cuda-scaling.sh
@@ -3,7 +3,7 @@ export OMP_NUM_THREADS=12
 
 echo "cuda\tomp"
 
-for it in 1000 5000 10000 15000 20000 25000 30000 35000 40000; do
+for it in 5000 10000 15000 20000 30000 40000 50000 60000; do
     echo -n "$it\t"
     echo -n "$( ./cuda-circles $it 20 | grep "Elapsed time:" | sed 's/Elapsed time: //' )\t"
     echo "$( ./omp-circles $it 20 | grep "Elapsed time:" | sed 's/Elapsed time: //' )"
